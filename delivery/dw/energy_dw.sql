7971207d61a207bc74e6176e209bdb5a0ae2330b
Simão Silva <email@simaosilva.com>
2024-10-20 12:16:11 +0100

    commit

--- delivery/dw/energy_dw.sql
+++ delivery/dw/energy_dw.sql
@@ -4,27 +4,27 @@ CREATE DATABASE energy_dw;
 
 USE energy_dw;
 
-CREATE TABLE dim_time {
+CREATE TABLE dim_time (
     time_id DATETIME PRIMARY KEY,
     year INT,
     season VARCHAR(6),
     month INT
-}
+);
 
-CREATE TABLE dim_location {
+CREATE TABLE dim_location (
     location_id INT AUTO_INCREMENT PRIMARY KEY,
     region VARCHAR(50),
     municipality VARCHAR(50),
     parish VARCHAR(50)
-}
+);
 
 -- Create the fact table for energy consumption
 CREATE TABLE fact_energy_consumption (
     fact_id INT AUTO_INCREMENT PRIMARY KEY,
-    time_id INT NOT NULL,
+    time_id DATETIME NOT NULL,
     location_id INT NOT NULL,
     energy_consumption DECIMAL(10,2) NOT NULL, -- Energy consumption in kWh
-    percentage_smart_meters DECIMAL(5,4) NOT NULL -- Percentage with 4 decimal places
+    percentage_smart_meters DECIMAL(5,4) NOT NULL, -- Percentage with 4 decimal places
     FOREIGN KEY (time_id) REFERENCES dim_time(time_id),
     FOREIGN KEY (location_id) REFERENCES dim_location(location_id)
 );
