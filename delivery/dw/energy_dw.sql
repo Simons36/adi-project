59f25a22b8586402eb9f5875ffe992b338b8d2d5
Simão Silva <email@simaosilva.com>
2024-10-20 11:53:20 +0100

    Added Task 4 data warehouse creation file

---
+++ delivery/dw/energy_dw.sql
@@ -0,0 +1,36 @@
+DROP DATABASE IF EXISTS energy_dw;
+
+CREATE DATABASE energy_dw;
+
+USE energy_dw;
+
+CREATE TABLE dim_time {
+    time_id DATETIME PRIMARY KEY,
+    year INT,
+    season VARCHAR(6),
+    month INT
+}
+
+CREATE TABLE dim_location {
+    location_id INT AUTO_INCREMENT PRIMARY KEY,
+    region VARCHAR(50),
+    municipality VARCHAR(50),
+    parish VARCHAR(50)
+}
+
+CREATE TABLE dim_smart_meters {
+    smart_meter_id INT AUTO_INCREMENT PRIMARY KEY,
+    percentage_smart_meters DECIMAL(5,4) NOT NULL -- Percentage with 4 decimal places
+}
+
+-- Create the fact table for energy consumption
+CREATE TABLE fact_energy_consumption (
+    fact_id INT AUTO_INCREMENT PRIMARY KEY,
+    time_id INT NOT NULL,
+    location_id INT NOT NULL,
+    smart_meter_id INT NOT NULL,
+    energy_consumption DECIMAL(10,2) NOT NULL, -- Energy consumption in kWh
+    FOREIGN KEY (time_id) REFERENCES dim_time(time_id),
+    FOREIGN KEY (location_id) REFERENCES dim_location(location_id),
+    FOREIGN KEY (smart_meter_id) REFERENCES dim_smart_meters(smart_meter_id)
+);
