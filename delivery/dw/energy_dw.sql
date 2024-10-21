7b8eab8a3c1a6bae3a7e2f0378fc7fc7662eafda
Simão Silva <email@simaosilva.com>
2024-10-21 13:07:00 +0100

    c

--- delivery/dw/energy_dw.sql
+++ delivery/dw/energy_dw.sql
@@ -5,7 +5,7 @@ CREATE DATABASE energy_dw;
 USE energy_dw;
 
 CREATE TABLE dim_time (
-    time_id INT AUTO_INCREMENT PRIMARY KEY,
+    time_id DATE PRIMARY KEY,
     year_id INT,
     season_id INT,             -- 1: Winter, 2: Spring, 3: Summer, 4: Autumn
     season_name VARCHAR(10),
@@ -23,7 +23,7 @@ CREATE TABLE dim_location (
 -- Create the fact table for energy consumption
 CREATE TABLE fact_energy_stats (
     fact_id INT AUTO_INCREMENT PRIMARY KEY,
-    time_id INT,
+    time_id DATE,
     location_id INT,
     energy_consumption DECIMAL(10,2), -- Energy consumption in kWh
     percentage_smart_meters DECIMAL(5,4), -- Percentage with 4 decimal places
